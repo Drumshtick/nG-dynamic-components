f40d79b68df0d61b25ca920cea138d0e251bdaf5
Drumshtick <ekodnar@outlook.com>
2022-02-05 21:41:24 -0600

    some basic styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faSitemap } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  faSitemap = faSitemap;
   constructor() { }
 
   ngOnInit(): void {
