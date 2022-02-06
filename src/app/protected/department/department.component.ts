2eb466aad445c078ed19f77310c3df60a272f67e
Drumshtick <ekodnar@outlook.com>
2022-02-06 14:42:46 -0600

    load dynamic components and rotate the components for display

--- src/app/protected/department/department.component.ts
+++ src/app/protected/department/department.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit, ViewChild, Type } from '@angular/core';
+import { DynamicDirective } from './dynamic.directive';
+import { DynamicComponent } from './dynamic.component';
+import { ClawesomeComponent } from './clawesome.component';
+import { PawesomeComponent } from './pawesome.component';
+import { SmileyComponent } from './smiley.component';
 @Component({
   selector: 'app-department',
   templateUrl: './department.component.html',
   styleUrls: ['./department.component.scss']
 })
-export class DepartmentComponent implements OnInit {
+export class DepartmentComponent implements OnInit, OnDestroy {
+  @ViewChild(DynamicDirective, { static: true }) private dynamicHost!: DynamicDirective;
+  private interval: number | undefined;
+  private currentIndex = 1;
 
+  private messages: { type: Type<DynamicComponent> }[] = [
+    { type: ClawesomeComponent },
+    { type: PawesomeComponent },
+    { type: SmileyComponent }
+  ]
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.loadComponent();
+    this.rotateMessages();
+  }
+
+  public ngOnDestroy(): void {
+      clearInterval(this.interval);
+  }
+
+  private loadComponent(): void {
+    if (this.messages.length === 0) return;
+    this.currentIndex = (this.currentIndex + 1) % this.messages.length;
+    const message = this.messages[this.currentIndex];
+
+    const viewContainerRef = this.dynamicHost.ViewContainerRef;
+    viewContainerRef.clear();
+
+    const componentRef = viewContainerRef.createComponent<DynamicComponent>(message.type);
+  }
+
+  private rotateMessages(): void {
+    this.interval = window.setInterval(() => {
+      this.loadComponent();
+    }, 10000);
   }
 
 }
