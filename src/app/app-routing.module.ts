0f4145a0f300ea7cb7c092686d95ee5418193246
Drumshtick <ekodnar@outlook.com>
2022-02-06 18:59:48 -0600

    protected ProtectedModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+// Import the authentication guard
+import { AuthGuard } from '@auth0/auth0-angular';
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'protected', loadChildren: () => import('./protected/protected.module').then(m => m.ProtectedModule) },
+  {
+    path: 'protected',
+    loadChildren: () => import('./protected/protected.module').then(m => m.ProtectedModule),
+    canActivate: [AuthGuard],// This is what protects the route
+  },
 ];
 
 @NgModule({
